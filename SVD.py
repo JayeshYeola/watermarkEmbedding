6066a3646ca1de7f05863d2c76e88d825e6d0260
Sagar Gabhane <sagar_gabhane@outlook.com>
2018-05-07 00:28:59 -0400

    SVD commit 2.

--- SVD.py
+++ SVD.py
@@ -1,9 +1,14 @@
 from scipy import linalg
+import SendereEnd
 import numpy as np
-m, n = 9, 6
-a = np.random.randn(m, n) + 1.j*np.random.randn(m, n)
-print "a"
-print a
-U, s, Vh = linalg.svd(a)
-print "shape"
-print U.shape,s.shape, Vh.shape
+#m, n = 9, 6
+#a = np.random.randn(m, n) + 1.j*np.random.randn(m, n)
+#print "a"
+#print a
+LL=SendereEnd.DWTofImage()
+Uc, Sc, Vc = linalg.svd(LL)
+#print "shape"
+#print Uc,Sc,Vc
+Snew=Sc+SendereEnd.alpha*SendereEnd.T
+Uw,Sw,Vw = linalg.svd(Snew)
+
