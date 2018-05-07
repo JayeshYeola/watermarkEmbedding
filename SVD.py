f5420e8d4db6517b5a9267f2ddf6f1656a9cbe6d
Sagar Gabhane <sagar_gabhane@outlook.com>
2018-05-07 00:44:26 -0400

    SVD commit 3.

--- SVD.py
+++ SVD.py
@@ -1,14 +1,14 @@
 from scipy import linalg
 import SendereEnd
-import numpy as np
-#m, n = 9, 6
-#a = np.random.randn(m, n) + 1.j*np.random.randn(m, n)
-#print "a"
-#print a
+
 LL=SendereEnd.DWTofImage()
 Uc, Sc, Vc = linalg.svd(LL)
-#print "shape"
-#print Uc,Sc,Vc
-Snew=Sc+SendereEnd.alpha*SendereEnd.T
+#print "Sc :"
+#print Sc
+Alpha = SendereEnd.alpha
+Wt=SendereEnd.GenScrambledWatermark()
+Snew=Sc+Alpha*Wt
+#print "Snew"
+#print Snew
 Uw,Sw,Vw = linalg.svd(Snew)
 
